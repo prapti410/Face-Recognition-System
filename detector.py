2d76e9cc9e04f253ff794ca32102644f7332184f
prapti410 <64392255+prapti410@users.noreply.github.com>
2022-06-28 13:49:15 +0530

    Face Recognition System

---
+++ detector.py
@@ -0,0 +1,33 @@
+import cv2
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "Classifiers/face.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+cam = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_SIMPLEX #Creates a font
+while True:
+    ret, im =cam.read()
+    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    faces=faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+    for(x,y,w,h) in faces:
+        nbr_predicted, conf = recognizer.predict(gray[y:y+h,x:x+w])
+        cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
+        if(nbr_predicted==1):
+             nbr_predicted='akansha'
+        elif(nbr_predicted==2):
+             nbr_predicted='vaibhav'
+        cv2.putText(im,str(nbr_predicted)+"--"+str(conf), (x,y+h),font, 1.1, (0,255,0)) #Draw the text
+        cv2.imshow('im',im)
+        
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
+
+
+
---
+++ trainer.py
@@ -0,0 +1,44 @@
+import cv2,os
+import numpy as np
+from PIL import Image 
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+cascadePath = "Classifiers/face.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+dataPath = 'dataSet'
+
+def get_images_and_labels(datapath):
+     image_paths = [os.path.join(datapath, f) for f in os.listdir(datapath)]
+    
+     images = []
+    
+     labels = []
+     for image_path in image_paths:
+         
+         image_pil = Image.open(image_path).convert('L')
+         
+         image = np.array(image_pil, 'uint8')
+         
+         nbr = int(os.path.split(image_path)[1].split(".")[0].replace("face-", ""))
+         
+         print(nbr)
+         
+         faces = faceCascade.detectMultiScale(image)
+         
+         for (x, y, w, h) in faces:
+             images.append(image[y: y + h, x: x + w])
+             labels.append(nbr)
+             cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
+             cv2.waitKey(10)
+     
+     return images, labels
+
+
+images, labels = get_images_and_labels(dataPath)
+cv2.imshow('test',images[0])
+cv2.waitKey(1)
+
+recognizer.train(images, np.array(labels))
+recognizer.save('trainer/trainer.yml')
+print("\ntraining complete.")
+cv2.destroyAllWindows()
